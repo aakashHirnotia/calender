0a5c7133266d72f9015201f797ab1761f5b6d527
aakashHirnotia <ahirnotia@ch.iitr.ac.in>
2020-12-26 16:17:11 +0530

    calender first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import InfiniteCalendar from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css'; // only needs to be imported once
+
+// Render the Calendar
+var today = new Date();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display: 'flex',alignItems: 'center',justifyContent: 'center'}}>
+      <InfiniteCalendar
+        width={800}
+        height={600}
+        selected={today}
+        displayOptions={{
+          showHeader: false,
+        }}
+        // disabledDays={[0,6]}
+        // minDate={lastWeek}
+        rowHeight={120}
+      />
     </div>
   );
 }
